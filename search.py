ea9341ecbdac25f7300052df6f7e912e4ea3414d
monjurkuet <34279740+monjurkuet@users.noreply.github.com>
2023-09-26 03:54:01 +0600

    Use selenium parsing

--- search.py
+++ search.py
@@ -2,22 +2,32 @@ import undetected_chromedriver as uc
 import json
 import sqlite3
 import time
-import cv2
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from io import BytesIO
-from PIL import Image
-import numpy as np
+import platform
+import re
+# Constants for file paths
+BROWSER_EXECUTABLE_PATH_WINDOWS = "C:\\Users\\muham\\AppData\\Local\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
+BROWSER_EXECUTABLE_PATH_LINUX = '/usr/bin/brave-browser'
 
-def GetDriver():
-   options = uc.ChromeOptions() 
-   options.add_argument('--proxy-server=127.0.0.1:24000')
-   caps = options.to_capabilities()
-   caps['goog:loggingPrefs'] = {'performance': 'ALL'} 
-   return uc.Chrome(headless=False,options=options,desired_capabilities=caps) 
+def new_browser():
+    options = uc.ChromeOptions()
+    caps = options.to_capabilities()
+    caps['goog:loggingPrefs'] = {'performance': 'ALL'}
+    SYSTEM_OS = platform.system()
+    browser_executable_path = (
+        BROWSER_EXECUTABLE_PATH_WINDOWS if SYSTEM_OS == 'Windows' else BROWSER_EXECUTABLE_PATH_LINUX
+    )
+    driver = uc.Chrome(
+        browser_executable_path=browser_executable_path,
+        headless=False,
+        options=options,
+        desired_capabilities=caps
+    )
+    return driver
 
 def clean_logs(driver,logs,target_url):
    for log in logs:
@@ -57,16 +67,7 @@ def ExtractData(driver,SEARCH_URL):
    driver.get_log("performance")  
    conn.commit()
 
-pixelRatio = 2
-def solve_blocked(browser, retry=3):
-    '''
-    Solve blocked
-    (Cross-domain iframe cannot get elements temporarily)
-    Simulate the mouse press and hold to complete the verification
-    '''
-    if not retry:
-        return False
-    element = None
+def solve_blocked(browser):
     try:
         element = WebDriverWait(browser,15).until(EC.presence_of_element_located((By.ID,'px-captcha')))
         # Wait for the px-captcha element styles to fully load
@@ -74,53 +75,17 @@ def solve_blocked(browser, retry=3):
     except BaseException as e:
         print(f'px-captcha element not found')
         return
-    print(f'solve blocked:{browser.current_url}, Retry {retry} remaining times')
-    template = cv2.imread(('captcha.png'), 0)
-    # Set the minimum number of feature points to match value 10
-    MIN_MATCH_COUNT = 8 
+    print(f'solve blocked:{browser.current_url}')
     if  element:
         print(f'start press and hold')
         ActionChains(browser).click_and_hold(element).perform()
         start_time = time.time()
         while 1:
-            if time.time() - start_time > 20:
-                break
-            x, y = element.location['x'], element.location['y']
-            width, height = element.size.get('width'), element.size.get('height')                
-            left = x * pixelRatio
-            top = y * pixelRatio
-            right = (x+width) * pixelRatio
-            bottom = (y+height) * pixelRatio
-            png = browser.get_screenshot_as_png() 
-            im = Image.open(BytesIO(png))
-            im = im.crop((left, top, right, bottom)) 
-            target = cv2.cvtColor(np.asarray(im),cv2.COLOR_RGB2BGR)  
-            # Initiate SIFT detector
-            sift = cv2.SIFT_create()
-            # find the keypoints and descriptors with SIFT
-            kp1, des1 = sift.detectAndCompute(template,None)
-            kp2, des2 = sift.detectAndCompute(target,None)
-            # create set FLANN match
-            FLANN_INDEX_KDTREE = 0
-            index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-            search_params = dict(checks = 50)
-            flann = cv2.FlannBasedMatcher(index_params, search_params)
-            matches = flann.knnMatch(des1,des2,k=2)
-            # store all the good matches as per Lowe's ratio test.
-            good = []
-            # Discard matches greater than 0.7
-            for m,n in matches:
-                if m.distance < 0.7*n.distance:
-                    good.append(m)
-            print( "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT))
-            if len(good)>=MIN_MATCH_COUNT:
-                print(f'release button')
+            if time.time() - start_time > random.uniform(6,10):
                 ActionChains(browser).release(element).perform()
                 return
-            time.sleep(0.5)
+            time.sleep(0.1)
     time.sleep(1)
-    retry -= 1
-    solve_blocked(retry)
 
 conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
@@ -128,9 +93,28 @@ cursor = conn.cursor()
 SEARCH_URL='https://www.walmart.com/browse/party-occasions/character-party-supplies/2637_8253261'+'?sort=best_seller&facet=retailer_type%3AWalmart'
 SEARCH_API_URL='https://www.walmart.com/orchestra/snb/graphql/Browse/'
 
-driver=GetDriver()
-driver.set_window_position(-2000,0)
+driver=new_browser()
 driver.get(SEARCH_URL)
 ExtractData(driver,SEARCH_URL)
 solve_blocked(driver)
 conn.close()
+
+
+all_products=driver.find_elements(By.XPATH,'//div[@data-item-id and @role="group"]')
+for product in all_products:
+    Product_Link=product.find_element(By.XPATH,'.//a[@link-identifier]').get_attribute('href').split('?')[0]
+    Title=product.find_element(By.XPATH,'.//span[@data-automation-id="product-title"]').text
+    Current_Price=product.find_element(By.XPATH,'.//span[contains(text(),"current price")]').text
+    Current_Price=re.search("\$[0-9]+(\.[0-9]+)?",Current_Price)[0]
+    try:
+        Original_Price=product.find_element(By.XPATH,'.//span[contains(text(),"Was $")]').text.split(' ')[-1]
+    except:
+        Original_Price=None
+    print(Original_Price)
+    Source_Link=SEARCH_URL
+    Image_Link=product.find_element(By.XPATH,'.//img').get_attribute('src').split('?')[0]
+    cursor.execute('''REPLACE INTO productlist (Source_Link, Title, Original_Price, Current_Price, Product_Link, Image_Link)
+                  VALUES (?, ?, ?, ?, ?, ?)''',
+                  (Source_Link, Title, Original_Price, Current_Price, Product_Link, Image_Link))
+    print(Title,Current_Price)
+conn.commit()
